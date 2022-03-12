730a1c397c853a0eb53794b9f116653784e14aea
Umaralikhon <umaralikhon@gmail.com>
2022-03-13 00:32:37 +0500

    Object #2

--- Objects/MyObjects.js
+++ Objects/MyObjects.js
@@ -58,3 +58,60 @@ function makeUserinfo(name, age) {
 
 let user = makeUserinfo("Alisa", 23);
 console.log(user);
+
+ 
+//Добавление свойства
+let userinfo5 = {
+    name: "Siri"
+}
+
+console.log("Name: " + userinfo5.name);
+
+userinfo5.company = "Apple";
+console.log(userinfo5);
+
+userinfo5["I like Java"] = true;
+console.log(userinfo5);
+
+userinfo5.address = {
+    country: "USA",
+    city: "New-York",
+}
+
+console.log(userinfo5);
+
+
+//Удаление свойства
+delete userinfo5.company;
+console.log(userinfo5);
+
+
+//Изменение объекта
+userinfo5.name = "Alexa";
+console.log(userinfo5);
+
+
+//Дублирование объекта
+let userinfo6 = Object.assign({}, userinfo5);
+userinfo6.name = "Alisa";
+
+console.log(userinfo5);
+console.log(userinfo6);
+
+
+//Добавление новых свойств при помощи Object.assign()
+Object.assign(userinfo6, {company: "Yandex"});
+console.log(userinfo6);
+
+
+//Проверка существование свойства
+
+//Проверка на существование объекта при помощи "?"
+if(userinfo6?.company) {
+    console.log(userinfo6.company);
+}
+
+//Проверка на существование объекта при помощи оператора "in"
+if("country" in userinfo6.address){
+    console.log(userinfo6.address.country);
+}
